284046af19a7bf5d124ada1b67a0892ae4dfc2d2
Aditya Yudis <adityayudis4@gmail.com>
2022-01-03 11:18:24 +0700

    membuat object type

---
+++ pertemuan 2/object-type.php
@@ -0,0 +1,38 @@
+<?php
+
+class Produk{
+    public  $judul = 'judul',
+            $penulis = 'penulis',
+            $penerbit = 'penerbit',
+            $harga = 0;
+
+    public function __construct( $judul="judul", $penulis="penulis", $penerbit="penerbit", $harga= 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel(){
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+class CetakInfoProduk{
+    public function cetak( Produk $produk){
+        $str = "{$produk->judul} | {$produk->getLabel()} ({$produk->harga})";
+        return $str;
+    }
+
+}
+$produk1 = new Produk( "Naruto", "Masashi Kishimoto", "Shonen Jump", 300000 );
+$produk2 = new Produk( "GTA 6", "Dan Houser", "RockStar", 666666 );
+$produk3 = new Produk( "Bleach" );
+
+echo "komik :" . $produk1->getLabel();
+echo "<br>";
+echo "game :" . $produk2->getLabel();
+echo "<br>";
+$infoProduk1 = new CetakInfoProduk();
+echo $infoProduk1->cetak($produk1);
